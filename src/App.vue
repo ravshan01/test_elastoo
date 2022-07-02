de694d7d5abbe86fca087f2783bb60054c479ab8
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-02 15:57:42 +0500

    update App component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+<template lang="pug">
+el-container.app
+  el-header.app-header
+    .app-header__actions
+      el-button(type="primary" @click="createGraphic") Create Graphic
+
+  el-main
+    router-view
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style lang="sass" src="./App.sass"></style>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    createGraphic() {},
+  },
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
