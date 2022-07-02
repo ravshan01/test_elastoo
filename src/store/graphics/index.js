cf0af8fb2888c439c05c6e857a5206211aeedd00
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-02 16:32:02 +0500

    create graphics vuex module

---
+++ src/store/graphics/index.js
@@ -0,0 +1,6 @@
+import Graphic from '@/models/Graphic.model'
+import VuexCrudModule from '@/models/VuexCrudModule'
+
+export default VuexCrudModule.create({
+  _class: Graphic,
+})
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore } from 'vuex'
 
+import graphics from '@/store/graphics'
+
 export default createStore({
   state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    graphics,
+  },
 })
