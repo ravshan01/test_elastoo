fb3a06f94e0734110574100eea49cbb72ac5ec9c
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-02 17:14:29 +0500

    update Graphic.model.js

--- src/models/Graphic.model.js
+++ src/models/Graphic.model.js
@@ -3,15 +3,24 @@ import apiService from '@/services/api.service.js'
 export default class Graphic {
   constructor(payload) {
     this.id = Math.random()
-    this.name = payload.name
-    this.type = payload.type
-    this.url = payload.url
-    this.data = payload.data
+    this.name = payload?.name || ''
+    this.type = payload?.type || 'line'
+    this.url = payload?.url || null
+    this.data = payload?.data || null
   }
   static types = ['line', 'pie']
 
-  static async create({ name, type, url }) {
-    const { data } = await apiService.get(url)
-    return new Graphic({ name, type, url, data })
+  /**
+   *
+   * @param { Object } payload тоже самое что передаём в конструктор
+   * @return { Promise<Graphic> }
+   */
+  static async create(payload) {
+    const isClassInstance = this instanceof Graphic
+
+    const { data } = await apiService.get(isClassInstance ? this.url : payload.url)
+    if (isClassInstance) this.data = data
+
+    return isClassInstance ? this : new Graphic({ ...payload, data })
   }
 }
