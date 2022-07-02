86a0eb02d41e3cc96ad4bc00fadfe7694f66b765
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-02 18:28:01 +0500

    realized GraphicsList component

---
+++ src/components/GraphicsList/index.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+.graphics
+  graphic(v-for="graphic in graphics" :key="graphic.id" :graphic="graphic")
+  .graphics__empty(v-show="!hasGraphics") Empty
+</template>
+<style lang="sass" src="./index.sass"></style>
+
+<script>
+import Graphic from '@/components/Graphic'
+
+export default {
+  name: 'GraphicsList',
+  components: { Graphic },
+  props: {
+    graphics: Array,
+    loading: Boolean,
+  },
+
+  computed: {
+    hasGraphics() {
+      return this.graphics.length
+    },
+  },
+}
+</script>
