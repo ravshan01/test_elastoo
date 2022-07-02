5ceb42f8b9fce0e101679806ac50069e5ceb85fd
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-02 15:24:06 +0500

    require ElementPlus in app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import { createApp } from 'vue'
+
+// TODO: так как тестовый проект - игнорирую большой вес бандла и импортирую сразу всё
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 import apiPlugin from '@/plugins/api.plugin.js'
 
-createApp(App).use(store).use(router).use(apiPlugin).mount('#app')
+createApp(App)
+  .use(ElementPlus, { size: 'small', zIndex: '9999' })
+  .use(store)
+  .use(router)
+  .use(apiPlugin)
+  .mount('#app')
