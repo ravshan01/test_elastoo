55b1275dcc7b4baa6843706028c351c82751f668
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-02 18:27:27 +0500

    realized Graphic component

---
+++ src/components/Graphic/index.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.graphic
+  line-graphic(v-if="graphic.type === $options.enum.graphicTypes.line" :graphic="graphic")
+  pie-graphic(v-if="graphic.type === $options.enum.graphicTypes.pie" :graphic="graphic")
+</template>
+<style lang="sass" src="./index.sass"></style>
+
+<script>
+import Graphic from '@/models/Graphic.model'
+import LineGraphic from '@/components/Graphic/LineGraphic'
+import PieGraphic from '@/components/Graphic/PieGraphic'
+
+export default {
+  name: 'Graphic',
+  components: { LineGraphic, PieGraphic },
+  enum: {
+    graphicTypes: {
+      line: Graphic.enum.types.line,
+      pie: Graphic.enum.types.pie,
+    },
+  },
+  props: {
+    graphic: [Graphic, Object],
+  },
+}
+</script>
